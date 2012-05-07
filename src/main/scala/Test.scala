80a557bb3d9a14b624c88f88dc121520b28c0c30
Brian Hsu <brianhsu.hsu@gmail.com>
2012-05-07 17:28:15 +0800

    Add UI for configure BlockNotification.

--- src/main/scala/Test.scala
+++ src/main/scala/Test.scala
@@ -11,6 +11,14 @@ import org.eclipse.swt._
 
 trait SWTHelper
 {
+    implicit def convertFromFont(font: Font): String = {
+        val fontData = (font.getFontData)
+        "%s / %d" format(fontData(0).getName, fontData(0).getHeight)
+    }
+
+    implicit def convertFromColor(color: Color): String = {
+        "(%d, %d, %d)".format(color.getRed, color.getGreen, color.getBlue)
+    }
 
     def createText(parent: Composite, title: String, style: Int = SWT.NONE) = 
     {
@@ -46,6 +54,114 @@ class IRCSetting(parent: Composite) extends Composite(parent, SWT.NONE) with SWT
     this.setLayout(gridLayout)
 }
 
+class BlockSetting(parent: Composite) extends Composite(parent, SWT.NONE) with SWTHelper
+{
+    var backgroundColor: Color = MyColor.Black
+    var fontColor: Color = MyColor.White
+    var messageFont: Font = Display.getDefault.getSystemFont
+
+    val gridLayout = new GridLayout(4, false)
+    val locationX = createText(this, "視窗位址 X：")
+    val locationY = createText(this, "視窗位址 Y：")
+    val width = createText(this, "視窗寬度：")
+    val height = createText(this, "視窗高度：")
+    val (bgLabel, bgButton) = createColorChooser("背景顏色：", backgroundColor = _)
+    val (fgLabel, fgButton) = createColorChooser("文字顏色：", fontColor = _)
+    val (fontLabel, fontButton) = createFontChooser("訊息字型：", messageFont = _)
+    val (aa, bb) = createTransparentChooser("透明度：")
+
+    def createTransparentChooser(title: String) =
+    {
+        val layoutData = new GridData(SWT.FILL, SWT.CENTER, true, false)
+        val layoutData2 = new GridData(SWT.FILL, SWT.CENTER, false, false)
+        val label = new Label(this, SWT.LEFT)
+        val slider = new Scale(this, SWT.HORIZONTAL)
+
+        def updateLabel()
+        {
+            label.setText(title + slider.getSelection + "%")
+        }
+
+        label.setLayoutData(layoutData2)
+        label.setText("透明度：")
+        slider.setMaximum(100)
+        slider.setMinimum(0)
+        slider.setLayoutData(layoutData)
+        slider.setSelection(20)
+        slider.addSelectionListener(new SelectionAdapter() {
+            override def widgetSelected(e: SelectionEvent) {
+                updateLabel()
+            }
+        })
+
+        updateLabel()
+
+        (label, slider)
+    }
+
+    def createFontChooser(title: String, action: Font => Any) =
+    {
+        val layoutData = new GridData(SWT.FILL, SWT.NONE, true, false)
+        val label = new Label(this, SWT.LEFT)
+        val button = new Button(this, SWT.PUSH)
+
+        label.setText(title)
+        button.setLayoutData(layoutData)
+        button.setText(messageFont)
+        button.addSelectionListener(new SelectionAdapter() {
+            override def widgetSelected(e: SelectionEvent) {
+                val fontDialog = new FontDialog(Main.shell)
+                val fontData = fontDialog.open()
+
+                if (fontData != null) {
+                    val font = new Font(Display.getDefault, fontData)
+                    action(font)
+                    button.setText(font)
+                }
+            }
+        })
+
+        (label, button)
+    }
+
+    def createColorChooser(title: String, action: Color => Any) = {
+        val layoutData = new GridData(SWT.FILL, SWT.NONE, true, false)
+        val label = new Label(this, SWT.LEFT)
+        val button = new Button(this, SWT.PUSH)
+
+        label.setText(title)
+        button.setLayoutData(layoutData)
+        button.setText(backgroundColor)
+        button.addSelectionListener(new SelectionAdapter() {
+            override def widgetSelected(e: SelectionEvent) {
+                val colorDialog = new ColorDialog(Main.shell)
+                val rgb = colorDialog.open()
+
+                if (rgb != null) {
+                    val color = new Color(Display.getDefault, rgb)
+                    action(color)
+                    button.setText(color)
+                }
+            }
+        })
+
+        (label, button)
+    }
+
+    this.setLayout(gridLayout)
+}
+
+class BalloonSetting(parent: Composite) extends Composite(parent, SWT.NONE) with SWTHelper
+{
+    val gridLayout = new GridLayout(4, false)
+    val locationX = createText(this, "視窗位址 X：")
+    val locationY = createText(this, "視窗位址 Y：")
+
+    this.setLayout(gridLayout)
+
+}
+
+
 object Main
 {
     val display = new Display
@@ -53,8 +169,10 @@ object Main
     var notification: NotificationBlock = null
 
     val stackLayout = new StackLayout
+    val displayStackLayout = new StackLayout
 
     val logginType = createLogginType()
+    val settingGroup = createSettingGroup()
     val ircButton = createIRCButton()
     val justinButton = createJustinButton()
     val settingPages = createSettingPages()
@@ -66,11 +184,44 @@ object Main
     val blockButton = createBlockButton()
     val balloonButton = createBalloonButton()
 
+    val displayPages = createDisplayPages()
+    val blockSetting = new BlockSetting(displayPages)
+    val balloonSetting = new BalloonSetting(displayPages)
+
+    def switchDisplayPages()
+    {
+        (blockButton.getSelection, balloonButton.getSelection) match {
+            case (true, _) => displayStackLayout.topControl = blockSetting
+            case (_, true) => displayStackLayout.topControl = balloonSetting
+        }
+
+        displayPages.layout()
+    }
+
+    def createDisplayPages() =
+    {
+        val composite = new Composite(shell, SWT.NONE)
+        val spanLayout = new GridData(SWT.FILL, SWT.NONE, true, false)
+        spanLayout.horizontalSpan = 2
+        composite.setLayoutData(spanLayout)
+        composite.setLayout(displayStackLayout)
+        composite
+    }
+
+    def createSettingGroup() =
+    {
+        val group = new Group(shell, SWT.SHADOW_NONE)
+        val spanLayout = new GridData(SWT.FILL, SWT.NONE, true, false)
+        group.setLayoutData(spanLayout)
+        group.setLayout(new RowLayout)
+        group
+    }
+
+
     def createDisplayGroup() =
     {
         val group = new Group(shell, SWT.SHADOW_NONE)
         val spanLayout = new GridData(SWT.FILL, SWT.NONE, true, false)
-        spanLayout.horizontalSpan = 2
         group.setLayoutData(spanLayout)
         group.setLayout(new RowLayout)
         group
@@ -88,6 +239,11 @@ object Main
         val button = new Button(displayGroup, SWT.RADIO)
         button.setText("固定區塊")
         button.setSelection(true)
+        button.addSelectionListener(new SelectionAdapter() {
+            override def widgetSelected(e: SelectionEvent) {
+                switchDisplayPages()
+            }
+        })
         button
     }
 
@@ -95,6 +251,11 @@ object Main
     {
         val button = new Button(displayGroup, SWT.RADIO)
         button.setText("泡泡通知")
+        button.addSelectionListener(new SelectionAdapter() {
+            override def widgetSelected(e: SelectionEvent) {
+                switchDisplayPages()
+            }
+        })
         button
     }
 
@@ -111,8 +272,8 @@ object Main
     def createSettingPages() = 
     {
         val composite = new Composite(shell, SWT.NONE)
-        val spanLayout = new GridData(SWT.FILL, SWT.FILL, true, true)
-        spanLayout.horizontalSpan = 3
+        val spanLayout = new GridData(SWT.FILL, SWT.NONE, true, false)
+        spanLayout.horizontalSpan = 2
         composite.setLayoutData(spanLayout)
         composite.setLayout(stackLayout)
         composite
@@ -127,7 +288,7 @@ object Main
 
     def createIRCButton() =
     {
-        val button = new Button(shell, SWT.RADIO)
+        val button = new Button(settingGroup, SWT.RADIO)
         button.setText("IRC")
         button.setSelection(true)
         button.addSelectionListener(new SelectionAdapter() {
@@ -140,7 +301,7 @@ object Main
     
     def createJustinButton() = 
     {
-        val button = new Button(shell, SWT.RADIO)
+        val button = new Button(settingGroup, SWT.RADIO)
         button.setText("Justin / Twitch")
         button.addSelectionListener(new SelectionAdapter() {
             override def widgetSelected(e: SelectionEvent) {
@@ -152,7 +313,7 @@ object Main
 
     def setupLayout()
     {
-        val gridLayout = new GridLayout(3,  false)
+        val gridLayout = new GridLayout(2,  false)
         shell.setLayout(gridLayout)
     }
 
@@ -160,6 +321,7 @@ object Main
     {
         setupLayout()
         switchSettingPages()
+        switchDisplayPages()
 
         shell.setText("IRC 聊天通知")
         shell.pack()
