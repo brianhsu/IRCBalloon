7b7bfef26de48ed050b9590c549b864f8262130b
Brian Hsu <brianhsu.hsu@gmail.com>
2012-05-09 16:40:53 +0800

    Fix error in build script.

--- createJAR.sh
+++ createJAR.sh
@@ -5,20 +5,20 @@ rm -rvf lib/swt* &&
 mkdir bin &&
 
 # Create JAR for Linux 64bit
-ln swt/swt-3.7.2-gtk-linux-x86_64.jar lib/ &&
+cp swt/swt-3.7.2-gtk-linux-x86_64.jar lib/swt.jar &&
 xsbt clean assembly &&
 cp target/IRCBalloon-assembly-$version.jar bin/IRCBalloon-linux64-$version.jar &&
 
 # Create JAR for Windows 64bit
-ln swt/swt-3.7.2-win32-win32-x86_64.jar lib/ &&
+cp swt/swt-3.7.2-win32-win32-x86_64.jar lib/swt.jar &&
 xsbt clean assembly &&
 cp target/IRCBalloon-assembly-$version.jar bin/IRCBalloon-win64-$version.jar &&
 
 # Create JAR for Windows 32bit
-ln swt/swt-3.7.2-win32-win32-x86.jar lib/ &&
+cp swt/swt-3.7.2-win32-win32-x86.jar lib/swt.jar &&
 xsbt clean assembly &&
 cp target/IRCBalloon-assembly-$version.jar bin/IRCBalloon-win32-$version.jar &&
 
 # Swtich to default SWT jar
 rm -rvf lib/swt* &&
-ln swt/swt-3.7.2-gtk-linux-x86_64.jar lib/
+cp swt/swt-3.7.2-gtk-linux-x86_64.jar lib/swt.jar
